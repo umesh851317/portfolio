6f370fe7987d3437e6550bc7349c4958c4ac3aa3
umesh851317 <230303089+umesh851317@users.noreply.github.com>
2026-07-21 15:45:58 +0530

    change project image path

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,10 +1,15 @@
+import medialertImg from "../assets/Medialert.png";
+import bookstoreImg from "../assets/book-Store.png";
+import urlShortnerImg from "../assets/url-shortner.png";
+
+
 const projects = [
        {
               id: 1,
               title: "MediAlert Smart Pharmacy ",
               description:
                      "pharmacy inventory management system with stock forecasting, expiry alerts, and analytics dashboard.",
-              image: "src/assets/Medialert.png",
+              image: medialertImg,
               tech: ["React", "Node.js", "Express", "MongoDB"],
               github: "https://github.com/umesh851317/MediAlert",
               live: "https://medi-alert-sigma.vercel.app/",
@@ -14,7 +19,7 @@ const projects = [
               title: "Book Store Web App",
               description:
                      "A full-stack e-commerce bookstore where users can browse books, search by category, add items to the cart, manage orders, and securely authenticate accounts.",
-              image: "src/assets/book-Store.png",
+              image: bookstoreImg,
               tech: ["React", "Next.js", "MongoDB", "Tailwind CSS"],
               github: "https://github.com/umesh851317/book-store",
               live: "https://book-store-drab-pi.vercel.app/",
@@ -24,7 +29,7 @@ const projects = [
               title: "URL Shortener (Backend)",
               description:
                      "A modern URL shortener with click analytics, visit history, and responsive dashboard.",
-              image: "src/assets/url-shortner.png",
+              image: urlShortnerImg,
               tech: ["React", "Node.js", "MongoDB", "Tailwind"],
               github: "https://github.com/umesh851317/url-shortner",
               live: "https://url-shortner-xi-blush.vercel.app/",
